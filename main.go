b45924dffef5a7baca6348f97ebb21c4621e7736
Dian Hansen <dian.hansen@hp.com>
2016-09-13 14:46:57 -0600

    add testing
    Signed-off-by: Dian Hansen <dian.hansen@hp.com>

--- main.go
+++ main.go
@@ -13,15 +13,12 @@ package main
 
 import (
     "github.com/hashicorp/terraform/plugin"
-    "github.com/hashicorp/terraform/terraform"
-    "github.com/HewlettPackard/oneview-golang/oneview"
+    "github.com/HewlettPackard/terraform-provider-oneview/oneview"
 )
 
 func main() {
   plugin.Serve(&plugin.ServeOpts{
-    ProviderFunc: func() terraform.ResourceProvider {
-      return oneview.Provider()
-    },
+  	ProviderFunc:	oneview.Provider,
   })
 }
  
