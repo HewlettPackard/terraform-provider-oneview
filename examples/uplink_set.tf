16721ca651987c3847f5c915d8114d385608c685
madhav-bharadwaj <madhavb@hpe.com>
2019-02-25 09:44:10 +0530

    updating examples
    Signed-off-by: madhav-bharadwaj <madhavb@hpe.com>

--- examples/uplink_set.tf
+++ examples/uplink_set.tf
@@ -1,22 +1,30 @@
 provider "oneview" {
-	ov_username = "administrator"
-	ov_password = "madhav123"
-	ov_endpoint = "https://10.170.16.44"
-	ov_sslverify = false
-	ov_apiversion = 800
-	ov_ifmatch = "*"
+  ov_username   = "administrator"
+  ov_password   = "madhav123"
+  ov_endpoint   = "https://10.170.16.41"
+  ov_sslverify  = false
+  ov_apiversion = 600
+  ov_ifmatch    = "*"
 }
 
 resource "oneview_uplink_set" "UplinkSet" {
-	name = "TestUplinkSet"
-	type = "uplink-setV4"
-	logical_interconnect_uri = "/rest/logical-interconnect-groups/0d3f98d6-0c10-4565-b9db-3907d5534e1d"
-	network_uris = []
-	fc_network_uris = []
-	fcoe_network_uris = []
-	port_config_infos = {}
-	manual_login_redistribution_state = "NotSupported"
-	connection_mode = "Auto"
-	network_type = "Ethernet"
-	ethernet_network_type = "Tagged"
+  name                     = "TestUplinkSet"
+  type                     = "uplink-setV4"
+  logical_interconnect_uri = "/rest/logical-interconnects/fe57cf01-91c1-4a1d-b057-dff3e881d2b9"
+  network_uris             = ["/rest/ethernet-networks/7771c389-5919-4037-8ff6-00aa593dbc4d"]
+  fc_network_uris          = [""]
+  fcoe_network_uris        = [""]
+  port_config_infos 	   = [{
+				    "desired_speed" = "Auto"
+				    "location" = {
+				      "location_entries" = {
+								"value"="1"
+								"type"="Bay"
+								}
+    					}
+  			     }]
+  manual_login_redistribution_state = "NotSupported"
+  connection_mode                   = "Auto"
+  network_type                      = "Ethernet"
+  ethernet_network_type             = "Tagged"
 }
