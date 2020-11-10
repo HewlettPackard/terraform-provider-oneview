15e5760113cbb8556f287b08823eb3e45e45cf08
asisbagga <asisbagga@gmail.com>
2020-11-11 00:13:55 +0530

    Extended Support to Interconnect Resources

--- examples/interconnect_type.tf
+++ examples/interconnect_type.tf
@@ -1,10 +1,10 @@
 provider "oneview" {
-        ov_username = "<ov-username>"
-        ov_password = "<ov-password>"
-        ov_endpoint = "<ov-endpoint>"
-        ov_sslverify = false
-        ov_apiversion = <ov_apiversion>
-        ov_ifmatch = "*"
+  ov_username =   "${var.username}"
+  ov_password =   "${var.password}"
+  ov_endpoint =   "${var.endpoint}"
+  ov_sslverify =  "${var.ssl_enabled}"
+  ov_apiversion = 2200
+  ov_ifmatch = "*"
 }
 
 data "oneview_interconnect_type" "interconnect_type" {
