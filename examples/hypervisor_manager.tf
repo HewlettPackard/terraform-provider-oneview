5ced48852529136fe088dfc6e381374a33f7a982
yuvirani <yuvaranisrec@gmail.com>
2021-01-15 13:35:15 +0530

    0.12 upgrade for hypervisor resources

--- examples/hypervisor_manager.tf
+++ examples/hypervisor_manager.tf
@@ -1,10 +1,10 @@
 provider "oneview" {
-	ov_username = "<username>"
-	ov_password = "<password>"
-	ov_endpoint = "<endpoint>"
-	ov_sslverify = false
-	ov_apiversion = <ov-apiversion>
-	ov_ifmatch = "*"
+  ov_username   = var.username
+  ov_password   = var.password
+  ov_endpoint   = var.endpoint
+  ov_sslverify  = var.ssl_enabled
+  ov_apiversion = 2400
+  ov_ifmatch    = "*"
 }
 
 data "oneview_scope" "scope_obj" {
@@ -12,38 +12,38 @@ data "oneview_scope" "scope_obj" {
 }
 
 resource "oneview_hypervisor_manager" "HypervisorManager" {
-	display_name = "TestHypervisorManager"
-        name = "<hypervisor_manager_hostname>"
-        username= "<hypervisor_manager_username>"
-        password = "<hypervisor_manager_password>"
-        port = <hypervisor_manager_port_num>
-	type = "HypervisorManagerV2"
-	initial_scope_uris = ["${data.oneview_scope.scope_obj.uri}"]
-
-
-}/* Uncomment following to run update
+  display_name       = "TestHypervisorManager"
+  name               = "<hypervisor_manager_hostname>"
+  username           = "<hypervisor_manager_username>"
+  password           = "<hypervisor_manager_password>"
+  port               = "<hypervisor_manager_port_num>"
+  type               = "HypervisorManagerV2"
+  initial_scope_uris = [data.oneview_scope.scope_obj.uri]
+}
+/*
 resource "oneview_hypervisor_manager" "HypervisorManager" {
-	display_name = "TestHypervisorManager_Renamed"
-        name = "<hypervisor_manager_hostname>"
-	type = "HypervisorManagerV2"
-        username= "dcs1"
-        preferences= {   type= "Vmware"
-			 virtual_switch_type= "Standard"
-			 distributed_switch_version=""
-			 distributed_switch_usage=""
-			 multi_nic_v_motion="false"
-			 drs_enabled="true"
-			 ha_enabled="false"
-	} 
+  display_name = "TestHypervisorManager_Renamed"
+  name         = "172.18.13.11"
+  type         = "HypervisorManagerV2"
+  username     = "dcs1"
+  preferences {
+    type                       = "Vmware"
+    virtual_switch_type        = "Standard"
+    distributed_switch_version = ""
+    distributed_switch_usage   = ""
+    multi_nic_v_motion         = "false"
+    drs_enabled                = "true"
+    ha_enabled                 = "false"
+  }
 }
 */
 /* Testing data source*/
 /*data "oneview_hypervisor_manager" "HypervisorManager" {
-        name = "<hypervisor_manager_hostname>"
+  name = "<hypervisor_manager_hostname>"
 }
 
 output "oneview_hypervisor_manager_value" {
-        value = "${data.oneview_hypervisor_manager.HypervisorManager.type}"
+  value = data.oneview_hypervisor_manager.HypervisorManager.type
 }
 */
 //Import existing resource
