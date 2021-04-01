e5d908011a253c81c58188b0d1480999fab4f69d
Nabhajit Ray <nabhajit.ray@hpe.com>
2021-04-01 12:09:30 +0530

    Changed Dockerfile and version.tf

--- versions.tf
+++ versions.tf
@@ -1,4 +1,9 @@
-
 terraform {
-  required_version = ">= 0.12"
+  required_version = ">= 0.13"
+  required_providers {
+    oneview = {
+      source = "HewlettPackard/oneview"
+      version = "6.0.0-13" #Check https://registry.terraform.io/v1/providers/hewlettpackard/oneview/versions
+    }
+  }
 }
