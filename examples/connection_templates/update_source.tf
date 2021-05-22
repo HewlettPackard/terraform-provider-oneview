cdf5e5d2b51676e9ff92482be09d748019bdd5d1
Shanmugam <m-.shanmugam@hpe.com>
2021-05-22 09:56:46 +0530

    added_conTemplate

---
+++ examples/connection_templates/update_source.tf
@@ -0,0 +1,17 @@
+provider "oneview" {
+  ov_username   = var.username
+  ov_password   = var.password
+  ov_endpoint   = var.endpoint
+  ov_sslverify  = var.ssl_enabled
+  ov_apiversion = 3000
+  ov_ifmatch    = "*"
+}
+
+resource "oneview_connection_templates" "connectionTemplates" {
+  name    = "defaultConnectionTemplate"
+  type    = "connection-template"
+  bandwidth {
+      maximum_bandwidth = 8000
+      typical_bandwidth = 2500
+  }
+}
