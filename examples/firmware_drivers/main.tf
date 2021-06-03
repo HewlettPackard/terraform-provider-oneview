e9d34257480a804bc7fe2f017fc35fb0d532fc3d
Shanmugam <m-.shanmugam@hpe.com>
2021-06-03 10:50:41 +0530

    sh

--- examples/firmware_drivers/main.tf
+++ examples/firmware_drivers/main.tf
@@ -6,11 +6,23 @@ provider "oneview" {
 	ov_apiversion = 3000
 	ov_ifmatch    = "*"
   }
-  
+
+data "oneview_firmware_drivers" "spp" {
+  id = "Synergy_Custom_SPP_2021_02_01_Z7550-97110"
+}
+
+data "oneview_firmware_drivers" "hotfix" {
+  id = "cp042959"
+}
+
+data "oneview_scope" "scope" {
+  name = "ScopTest"
+}
+
 # Creating Custom Firmware Service Pack
 resource "oneview_firmware_drivers" "drivers" {
-  baseline_uri = "/rest/firmware-drivers/Synergy_Custom_SPP_2021_02_01_Z7550-97110"
-  hotfix_uris = ["/rest/firmware-drivers/cp042959"]
+  baseline_uri = data.oneview_firmware_drivers.spp.uri
+  hotfix_uris = [data.oneview_firmware_drivers.hotfix.uri]
   custom_baseline_name = "Terraform_SPP"
-  initial_scope_uris = ["/rest/scopes/2f4cbc4f-a1a2-4073-bdf4-829041b3398b", "/rest/scopes/bda31962-f4e5-4969-b036-bc0c63b0ccd3"]
+  initial_scope_uris = [data.oneview_scope.scope.uri]
 }
