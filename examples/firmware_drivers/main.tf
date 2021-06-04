6aa1b21443b687294dc1cae8d83189c3734f5d90
Shanmugam <m-.shanmugam@hpe.com>
2021-06-04 12:08:57 +0530

    used_version_variable

--- examples/firmware_drivers/main.tf
+++ examples/firmware_drivers/main.tf
@@ -3,7 +3,7 @@ provider "oneview" {
 	ov_password   = var.password
 	ov_endpoint   = var.endpoint
 	ov_sslverify  = var.ssl_enabled
-	ov_apiversion = 3000
+	ov_apiversion = var.api_version
 	ov_ifmatch    = "*"
   }
 
