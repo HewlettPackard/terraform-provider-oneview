f2b213ef4ae70d3f61b22a386dcd4ca82e1c52de
Nabhajit Ray <nabhajit.ray@hpe.com>
2021-06-21 11:08:26 +0530

    Added snmp v3 user example

---
+++ examples/appliance_snmpv3_user/import.tf
@@ -0,0 +1,12 @@
+provider "oneview" {
+  ov_username   = var.username
+  ov_password   = var.password
+  ov_endpoint   = var.endpoint
+  ov_sslverify  = var.ssl_enabled
+  ov_apiversion = var.api_version
+  ov_ifmatch    = "*"
+}
+
+#Importing Existing resource
+resource "oneview_appliance_snmpv3_user" "snmpvuser" {
+ }
