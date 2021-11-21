56c4dc451e722b4cc5d4252234df1da6f243b596
Nabhajit Ray <nabhajit.ray@hpe.com>
2021-11-21 09:40:15 +0530

    OV 6.4 validation

---
+++ examples/connection_templates/update_resource.tf
@@ -0,0 +1,16 @@
+provider "oneview" {
+  ov_username   = var.username
+  ov_password   = var.password
+  ov_endpoint   = var.endpoint
+  ov_sslverify  = var.ssl_enabled
+  ov_apiversion = 3000
+  ov_ifmatch    = "*"
+}
+
+resource "oneview_connection_templates" "connectionTemplates" {
+  name = "renamed-connection-template"
+  bandwidth {
+      maximum_bandwidth = 8000
+      typical_bandwidth = 2500
+  }
+}
