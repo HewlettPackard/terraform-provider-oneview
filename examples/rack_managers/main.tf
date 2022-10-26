1d5e9d99c5c40fba73b7fdb9dccfe7e376f17133
nabhajit-ray <nabhajit.ray@hpe.com>
2022-10-26 16:47:06 +0530

    Support rack manager

---
+++ examples/rack_managers/main.tf
@@ -0,0 +1,20 @@
+provider "oneview" {
+  ov_username   = var.username
+  ov_password   = var.password
+  ov_endpoint   = var.endpoint
+  ov_sslverify  = var.ssl_enabled
+  ov_apiversion = var.api_version
+  ov_ifmatch    = "*"
+}
+
+data "oneview_scope" "scope_obj" {
+  name = "testing"
+}
+# Add the Rack Manager 
+resource "oneview_rack_manager" "RM" {
+  hostname               = "<ipaddress>"
+  username               = "<user>"
+  password               = "<password>"
+  initial_scope_uris = [data.oneview_scope.scope_obj.uri]
+  
+}
