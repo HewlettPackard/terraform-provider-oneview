f4fc59ae49d79f847f9ac7023d059cc1cef20015
akshith-gunasheelan <126816102+akshith-gunasheelan@users.noreply.github.com>
2023-04-25 15:33:47 +0530

    Update main.tf

--- examples/firmware_drivers/main.tf
+++ examples/firmware_drivers/main.tf
@@ -8,20 +8,20 @@ provider "oneview" {
   }
 
 data "oneview_firmware_drivers" "spp" {
-  id = "Synergy_Custom_SPP_2021_02_01_Z7550-97110"
+  id = "Synergy_Custom_SPP_2021.02.01_Z7550-97110"
 }
 
 data "oneview_firmware_drivers" "hotfix" {
-  id = "cp042959"
+  id = "HPE ProLiant Gen10 and Gen10 Plus Smart Array Controller Driver for VMware vSphere 6.5 (Driver Component)."
 }
 
 data "oneview_scope" "scope" {
-  name = "ScopTest"
+  name = "Auto-Scope"
 }
 
 # Creating Custom Firmware Service Pack
 resource "oneview_firmware_drivers" "drivers" {
-  baseline_uri = data.oneview_firmware_drivers.spp.uri
+  baseline_uri = "data.oneview_firmware_drivers.spp.uri"
   hotfix_uris = [data.oneview_firmware_drivers.hotfix.uri]
   custom_baseline_name = "Terraform_SPP"
   initial_scope_uris = [data.oneview_scope.scope.uri]
