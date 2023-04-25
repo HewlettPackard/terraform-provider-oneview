4a0e1b8f63bca1753adf1c8ed7b4290314baa80b
akshith-gunasheelan <126816102+akshith-gunasheelan@users.noreply.github.com>
2023-04-25 15:04:59 +0530

    Update data_source.tf

--- examples/uplink_sets/data_source.tf
+++ examples/uplink_sets/data_source.tf
@@ -7,26 +7,15 @@ provider "oneview" {
   ov_ifmatch    = "*"
 }
 
-# Fetching Logical Interconnect
-data "oneview_logical_interconnect" "logical_interconnect" {
-  name = "LE-LIG"
+# Example for data source
+data "oneview_uplink_set" "uplink_set" {
+  name = "Auto-UplinkSet-Updated"
 }
 
-# Fetching Network
-data "oneview_ethernet_network" "ethernetnetwork" {
-  name = "Auto_Ethernet_1"
+output "oneview_uplink_set_value" {
+  value = data.oneview_uplink_set.uplink_set.uri
 }
 
-# Updates Uplink Set
-resource "oneview_uplink_set" "UplinkSet" {
-  name                              = "Auto-UplinkSetup-Updated"
-  type                              = "uplink-setV7"
-  logical_interconnect_uri          = data.oneview_logical_interconnect.logical_interconnect.uri
-  network_uris                      = [data.oneview_ethernet_network.ethernetnetwork.uri]
-  fc_network_uris                   = []
-  fcoe_network_uris                 = []
-  manual_login_redistribution_state = "NotSupported"
-  connection_mode                   = "Auto"
-  network_type                      = "Ethernet"
-  ethernet_network_type             = "Tagged"
-}
+#Importing Existing resource
+#resource "oneview_uplink_set" "import_us"{
+#}
