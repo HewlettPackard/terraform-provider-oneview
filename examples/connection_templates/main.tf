8907afe7b7acde092dc4ef34ea2caf99b57ae0e3
akshith-gunasheelan <126816102+akshith-gunasheelan@users.noreply.github.com>
2023-04-25 14:33:45 +0530

    Update main.tf

--- examples/connection_templates/main.tf
+++ examples/connection_templates/main.tf
@@ -7,19 +7,13 @@ provider "oneview" {
   ov_ifmatch    = "*"
 }
 
-# To update typical and maximum bandwidth values of any network, you will need to fetch the respective connection template uri from a network as below: 
-/*
 data "oneview_ethernet_network" "network" {
-  name = "test-network"
+  name = "Auto_Ethernet_1"
 }
 output "connection_template_uri" {
   value = data.oneview_ethernet_network.network.connection_template_uri
 }
-*/
-# Then you will need to import the connection template and update typical and maximum bandwidth values. 
-# To import an existing connection template, retain this configuration and run below command. 
-# $ terraform import oneview_connection_templates.connectionTemplates <connection-template-uri>
-# Then just update the update_resource.tf with your desired values and execute it.
 
-resource "oneview_connection_templates" "connectionTemplates" {
-}
+
+#resource "oneview_connection_templates" "connectionTemplates" {
+#}
