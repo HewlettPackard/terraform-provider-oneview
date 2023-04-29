c5c824c7f6b10ca686e62a5c1ae761de336f9681
nabhajit-ray <nabhajit.ray@hpe.com>
2023-04-29 22:51:01 +0530

    connection templates comments chaanges

--- examples/connection_templates/main.tf
+++ examples/connection_templates/main.tf
@@ -14,11 +14,11 @@ output "connection_template_uri" {
   value = data.oneview_ethernet_network.network.connection_template_uri
 }
 
-# Then you will need to import the connection template and update typical and maximum bandwidth values. 
-# To import an existing connection template, retain this configuration and run below command. 
-# $ terraform import oneview_connection_templates.connectionTemplates #connection-template-uri#
-# Then just update the update_resource.tf with your desired values and execute it.
-
+/*Then you will need to import the connection template and update typical and maximum bandwidth values. 
+To import an existing connection template, retain this configuration and run below command. 
+$ terraform import oneview_connection_templates.connectionTemplates #connection-template-uri#
+Then just update the update_resource.tf with your desired values and execute it.
+*/
 
 #resource "oneview_connection_templates" "connectionTemplates" {
 #}
